0d40b9773cb3f7d865a18ed4c2d39dd42194922e
Ryan Kadel <ryan.kadel88@gmail.com>
2020-01-04 23:23:33 -0500

    checkpoint 14

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,8 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('First Post', now(), 'First article!!'),
+    ('Second Post', now(), 'Second article!!'),
+    ('Third Post', now(), 'Third article!!'),
+    ('Fourth Post', now(), 'Fourth article!!'),
+    ('Fifth Post', now(), 'Fifth article!!')
+;
